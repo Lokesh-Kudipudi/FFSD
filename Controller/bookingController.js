fc6e5501016c52eee6b320f15d028a0f23acdb78
Lokesh-Kudipudi <lokeshkudipudi2006@gmail.com>
2025-04-08 11:12:13 +0530

    Added booking controller

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -68,7 +68,116 @@ async function getHotelBookings(hotelId) {
   }
 }
 
+async function makeTourBooking(userId, tourId, bookingDetails) {
+  try {
+    // Validate the tour exists
+    const tour = await Tour.findById(tourId);
+    if (!tour) {
+      throw new Error("Tour not found.");
+    }
+    // Create a new booking object
+    const booking = new Booking({
+      userId,
+      type: "Tour",
+      itemId: tourId,
+      bookingDetails: {
+        ...bookingDetails,
+        status: bookingDetails.status || "pending", // Default to pending if no status provided
+        bookingDate: new Date(),
+        price:
+          tour.price.amount -
+          tour.price.discount * tour.price.amount,
+      },
+    });
+
+    const savedBooking = await booking.save();
+
+    return {
+      status: "success",
+      data: {
+        booking: savedBooking,
+      },
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+}
+
+async function makeHotelBooking(
+  userId,
+  hotelId,
+  bookingDetails
+) {
+  try {
+    // Validate the hotel exists
+    const hotel = await Hotel.findById(hotelId);
+    if (!hotel) {
+      throw new Error("Hotel not found.");
+    }
+    // Create a new booking object
+    const booking = new Booking({
+      userId,
+      type: "Hotel",
+      itemId: hotelId,
+      bookingDetails: {
+        ...bookingDetails,
+        status: bookingDetails.status || "pending", // Default to pending if no status provided
+        bookingDate: new Date(),
+      },
+    });
+
+    const savedBooking = await booking.save();
+
+    return {
+      status: "success",
+      data: {
+        booking: savedBooking,
+      },
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+}
+
+async function cancelBooking(bookingId) {
+  try {
+    const result = await Booking.updateOne(
+      { _id: bookingId, "bookingDetails.status": "pending" },
+      { $set: { "bookingDetails.status": "cancel" } }
+    );
+
+    if (result.modifiedCount === 1) {
+      console.log("Booking status updated to cancel.");
+
+      return {
+        status: "success",
+        message: "Booking cancelled successfully.",
+      };
+    } else {
+      console.log(
+        "No pending booking found or already updated."
+      );
+
+      return {
+        status: "error",
+        message: "Booking not found or already cancelled.",
+      };
+    }
+  } catch (error) {
+    console.error("Error updating booking status:", error);
+  }
+}
+
 module.exports = {
   getUserBookings,
   getHotelBookings,
+  makeTourBooking,
+  makeHotelBooking,
+  cancelBooking,
 };
